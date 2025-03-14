6c74799b25a42dad0857f9ffba96cce7e233140e
Thanhmai1 <test@w3schools.com>
2025-03-14 08:31:35 +0700

    create project

---
+++ BJWA-exam/src/main/java/com/example/bjwaexam/BjwaExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.bjwaexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjwaExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BjwaExamApplication.class, args);
+    }
+
+}
