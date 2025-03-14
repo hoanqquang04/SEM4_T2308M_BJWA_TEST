8b4786483b556feaad8665b0aa592b8f6f8e3113
HoangHieu <hoangminhhieu230805@gmail.com>
2025-03-14 09:11:40 +0700

    add @Service va them cau hinh CORS

--- demo/src/main/java/com/example/demo/services/UserService.java
+++ demo/src/main/java/com/example/demo/services/UserService.java
@@ -3,9 +3,11 @@ package com.example.demo.services;
 import com.example.demo.entities.User;
 import com.example.demo.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserService implements IUserService {
     @Autowired
     private IUserRepository userRepository;
